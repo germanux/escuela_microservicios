a12335526a5226d367809e9fbf5122ca719c137c
german <german.caballero@sinensia.com>
2019-12-13 17:59:54 +0100

    Leer todos y obtener uno

--- api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/entidades/Usuario.java
+++ api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/entidades/Usuario.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Usuario implements Serializable {
+public class Usuario /*implements Serializable*/ {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
--- api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/restapi/UsuariosController.java
+++ api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/restapi/UsuariosController.java
@@ -1,7 +1,12 @@
 package com.viewnext.apiusuarios.restapi;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,23 +17,33 @@ import com.viewnext.apiusuarios.entidades.Usuario;
 import com.viewnext.apiusuarios.model.AlmacenDAOUsuarios;
 
 @RestController()
-@RequestMapping("/api")
+@RequestMapping("/api/usuarios")
 public class UsuariosController {
 
 	// Inyección de depencias: Spring se encarga de
 	// Instanciar el DAO (obj, no interfaz, y asignarlo
 	// a nuestro RestController al contruirlo
-	@Autowired 
+	@Autowired
 	private AlmacenDAOUsuarios dao;
 	
-	@RequestMapping(path = "/usuarios", method = {RequestMethod.GET /*, RequestMethod.POST */} )
-	public Usuario getUsuario() {
-		return new Usuario(-1, "Ok", "email", "111");
-	}
-	@PostMapping("/usuarios")
+	
+	@PostMapping()
 	public Usuario crearUsuario(@RequestBody Usuario usuario) {	
 		// Recibe sin ID en el BODY de la petición HTTP y deserializa el JSON a un obj Usuario
 		return dao.save(usuario);	// Devuelve con ID
+	}
+	
+	@GetMapping
+	public List<Usuario> leerTodos() {
+		return dao.findAll();
+	}
+	
+	@RequestMapping(value="/{id}", method = {RequestMethod.GET /*, RequestMethod.POST */} )
+	public Usuario getUsuario(@PathVariable Integer id) {
+		System.out.println(">>>> ID RECIBIDO " + id);
+		//TODO Optional
+		Optional<Usuario> usu = dao.findById(id);
+		return usu.orElse(null);
 	}
 }
 

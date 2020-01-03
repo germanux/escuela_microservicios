2b4b590ace56f85230742c3c6c0605cf8d116459
german <german.caballero@sinensia.com>
2020-01-03 13:54:36 +0100

    app con componente mínimo

---
+++ app-vacia/src/app/app.componente.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({ 
+    selector: 'app-comp-raiz',
+    template: '<h2>Aquí estoy</h2>'
+})
+export /*public*/ class AppComponente {
+    
+}

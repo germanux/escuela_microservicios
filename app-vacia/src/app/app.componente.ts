c025532ee04e38b7a98a1e4073d2fcd4b594335b
german <german.caballero@sinensia.com>
2020-01-03 18:03:43 +0100

    Componente vacío

--- app-vacia/src/app/app.componente.ts
+++ app-vacia/src/app/app.componente.ts
@@ -5,5 +5,13 @@ import { Component } from '@angular/core';
     template: '<h2>Aquí estoy</h2>'
 })
 export /*public*/ class AppComponente {
-    
+    contador = 0;
+
+    ngOnInit() {
+        this.contador = 7;
+    }
+    alPulsarBtn() {
+        this.contador ++;
+        console.log("Contador = " + this.contador);
+    }
 }
